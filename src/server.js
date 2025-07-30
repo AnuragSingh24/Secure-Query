b98524a60ea194a046cf94695d55d27d457afa21
Anurag Singh <145691945+AnuragSingh24@users.noreply.github.com>
2025-07-31 00:03:05 +0530

    Update server.js

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-  origin: 'https://secure-query-ai.vercel.app/',
+  origin: 'https://secure-query-ai.vercel.app',
   credentials: true
 }));
 
